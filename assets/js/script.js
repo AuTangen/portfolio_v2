7976613b7492ebc62d8fabcef31773d3383f5fe5
AuTangen <atangen2406@gmail.com>
2023-06-17 10:32:21 -0400

    increased bottom margin for email form

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,16 +98,3 @@ noteContainer.addEventListener('mouseleave', function () {
 });
 
 
-// -------mobile nav close---------
-const navLinks = document.querySelectorAll('.nav-link')
-const menuToggle = document.getElementById('offcanvasDarkNavbar')
-const bsCollapse = new bootstrap.Collapse(menuToggle, {
-  toggle: false
-})
-navLinks.forEach((l) => {
-    l.addEventListener('click', () => {
-        if(bsCollapse._isShown()){
-            bsCollapse.hide()
-        }
-    })
-})
