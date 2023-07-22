02dbd0c77a4e8ee6e5f1d92299a0737692e612e3
AuTangen <atangen2406@gmail.com>
2023-07-22 13:38:15 -0400

    updated resume/added scroll transition effects

---
+++ assets/js/scroll.js
@@ -0,0 +1,19 @@
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+        else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+
+
+
+
+const scrollElement = document.querySelectorAll('.hidden')
+
+scrollElement.forEach((el) => observer.observe(el))
